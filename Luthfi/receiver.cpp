85a8a4d0b56cf590e5251bdc945cc743d2e3d4b0
luthkur <13514102@std.stei.itb.ac.id>
2016-11-27 12:34:57 +0700

    Added ACK dan NAK sending capabilities to receiver

---
+++ Luthfi/receiver.cpp
@@ -0,0 +1,277 @@
+/*
+* File : receiver.cpp
+* Author : Joshua K - 012, Albertus K - 100, Luthfi K -102
+*/
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+#include "dcomm.h"
+#include "crc16.h"
+
+/* Define receive buffer size */
+#define RXQSIZE 6 + VARLEN
+#define UPPERLIMIT 6
+#define LOWERLIMIT 3
+
+/** FRAME THING **/
+#define VARLEN 6
+char frame[1 + 1 + 1 + VARLEN + 1 + 2] = "";
+
+int getEtx (char* frame) {
+	int i = 3;
+	while ((frame[i] != ETX)&&(i < RXQSIZE)) {
+		i++;
+	}
+	return i;
+}
+
+bool checkSum (char* frame) {
+	char text[VARLEN];
+	memset(text, 0, sizeof text);
+	char chks[2];
+	for (int i = 3;i < getEtx(frame);i++) {
+		text[i-3] = frame[i];
+	}
+	unsigned short ichks = calc_crc16(text, strlen(text));
+	chks[0] = ichks & 0xff;
+	chks[1] = (ichks >> 8) & 0xff;
+	printf("Checksum : should be %d-%d ; get %d-%d\n", (int) chks[0], (int) chks[1], frame[getEtx(frame)+1], frame[getEtx(frame)+2]);
+	if ((frame[getEtx(frame)+1] != chks[0]) || (frame[getEtx(frame)+2] != chks[1])) {
+		return false;
+	} else {
+		return true;
+	}
+}
+
+/* Delay to adjust speed of consuming buffer, in milliseconds */
+#define DELAY 600000
+
+/* Message */
+Byte x_msg[1];
+char r_msg[1];
+
+/* Buffer */
+Byte rxbuf[RXQSIZE];
+QTYPE rcvq = {0, 0, 0, RXQSIZE, rxbuf };
+QTYPE *rxq = &rcvq;
+Byte sent_xonxoff = XON;
+bool send_xon = false, send_xoff = false, end = false, childead = false;
+FILE* pipein_fp;
+char readbuf[80];
+
+/* Server-Client socket address */
+struct sockaddr_in sserver, sclient;
+socklen_t cli_len = sizeof(sclient);
+
+/* Socket */
+int sockfd; // listen on sock_fd
+
+/* Functions declaration */
+static Byte *rcvchar(int sockfd, QTYPE *queue);
+static Byte *q_get(QTYPE *, Byte *);
+static void *consume(void *param);
+
+void sendNAK(int frameNum, int udpSocket) {
+	char frame[1 + 1 + 2] = "";
+	char chks[3];
+	frame[0] = NAK;
+	frame[1] = frameNum;
+	unsigned short ichks = calc_crc16(frame, strlen(frame));
+	chks[0] = ichks & 0xff;
+	chks[1] = (ichks >> 8) & 0xff;
+	chks[2] = 0;
+	strcat(frame, chks);
+	printf("NAK %d\n", frameNum);
+	sendto(udpSocket, frame, strlen(frame), 0, (struct sockaddr *)&sclient,sizeof(sclient));
+}
+
+void sendENQ(int frameNum, int udpSocket) {
+	char frame[1 + 1 + 2] = "";
+	char chks[3];
+	frame[0] = NAK;
+	frame[1] = frameNum;
+	unsigned short ichks = calc_crc16(frame, strlen(frame));
+	chks[0] = ichks & 0xff;
+	chks[1] = (ichks >> 8) & 0xff;
+	chks[2] = 0;
+	strcat(frame, chks);
+	printf("ENQ %d\n", frameNum);
+	sendto(udpSocket, frame, strlen(frame), 0, (struct sockaddr *)&sclient,sizeof(sclient));
+}
+
+
+int main(int argc, char *argv[])
+{
+	Byte c;
+	/* bind socket to the port number given in argv[1].
+	*/
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sockfd < 0)
+    {
+        puts("Could not create socket");
+        return 1;
+    }
+
+	sserver.sin_family = AF_INET;
+	sserver.sin_addr.s_addr = INADDR_ANY;
+	sserver.sin_port = htons(atoi(argv[1]));
+
+	if (bind(sockfd,(struct sockaddr *)&sserver, sizeof(sserver)) < 0)
+    {
+        perror("Bind failed. Error");
+        return 1;
+    }
+	// Get ip address
+	pipein_fp = popen("hostname -I","r");
+	char* ip;
+	fgets(ip, 13, pipein_fp);
+	pclose(pipein_fp);
+	printf("Binding pada %s %d...\n", ip, atoi(argv[1]));
+
+	/* Initialize XON/XOFF flags */
+	sent_xonxoff = XON;
+	send_xon = true, send_xoff = false;
+
+	/* Create child process */
+	int j=0;
+	pthread_t consume_thread;
+
+	if(pthread_create(&consume_thread, NULL, consume, &rcvq)){
+		fprintf(stderr, "Error creating thread\n");
+		return 1;
+	}
+
+	/*** IF PARENT PROCESS ***/
+	while (true) {
+		c = *(rcvchar(sockfd, rxq));
+		/* Quit on end of file */
+		if (end) {
+			while (true) {
+				if(childead)
+				{
+					exit(0);
+				}
+			}
+		}
+		else {
+				if (!c) {
+					j = 1;
+				} else {
+					j++;
+			}
+			printf("Menerima byte ke-%d.\n", j);
+		}
+	}
+
+	if(pthread_join(consume_thread,NULL)){
+		fprintf(stderr,"Error joining thread\n");
+		return 2;
+	}
+}
+
+static Byte *rcvchar(int sockfd, QTYPE *queue)
+{
+	/**
+	Read a character from socket and put it to the receive buffer. If the number of characters in the
+	* receive buffer is above certain level, then send XOFF and set a flag
+	*/
+	if ((queue->count >= UPPERLIMIT)&&(!send_xoff)) { //buffer full
+		sent_xonxoff = XOFF;
+		send_xon = false;
+		send_xoff = true;
+
+		x_msg[0] = sent_xonxoff;
+
+		// send 'sent_xonxoff' via socket
+		if (sendto(sockfd, x_msg, 1, 0, (struct sockaddr *)&sclient,sizeof(sclient)) > 0){
+			puts("Buffer > minimum upperlimit.\nMengirim XOFF.");
+		}
+		else {
+			puts("XOFF gagal dikirim");
+		}
+	}
+
+	// read char from socket (receive)
+	if (recvfrom(sockfd, frame, RXQSIZE, 0, (struct sockaddr *)&sclient, &cli_len) < 0)
+		puts("Receive byte failed");
+
+	sendENQ(frame[1],sockfd);
+	sendNAK(frame[1],sockfd);
+	printf("what we got %s, framenum %d, checksum %c%c\n", frame, (int)frame[1], frame[getEtx(frame)+1], frame[getEtx(frame)+2]);
+	printf(">>>>>> Checksum result is %d\n", checkSum(frame));
+	// check end of file
+	if(frame[0] == Endfile) {
+		puts("Received end of file");
+		end = true;
+		return queue->data;
+	}
+	else {
+	// transfer from received msg to buffer
+
+	queue->data[queue->rear] = r_msg[0];
+
+	// increment rear index, check for wraparound, increased buffer content size
+	queue->rear += 1;
+	if (queue->rear >= RXQSIZE) queue->rear -= RXQSIZE;
+	queue->count += 1;
+
+	return queue->data;
+	}
+}
+
+static Byte *q_get(QTYPE *queue, Byte *data)
+{
+	Byte *current;
+	/* Nothing in the queue */
+	if (!queue->count) return (NULL);
+
+	/** send XON **/
+	if ((queue->count <= LOWERLIMIT) && (!send_xon)){
+		sent_xonxoff = XON;
+		send_xon = true;
+		send_xoff = false;
+		x_msg[0] = sent_xonxoff;
+
+		if (sendto(sockfd, x_msg, 1, 0, (struct sockaddr *)&sclient,sizeof(sclient)) > 0)
+			puts("Buffer < maximum lowerlimit.\nMengirim XON.");
+		else
+			puts("XON gagal dikirim");
+	}
+	//select current data from buffer
+	current = &queue->data[queue->front];
+
+	//increment front index, check for wraparound, reduced buffer content size
+	queue->front += 1;
+	if (queue->front >= RXQSIZE) queue->front -= RXQSIZE;
+	queue->count -= 1;
+
+	return current;
+}
+
+static void* consume(void *queue){
+
+	QTYPE *rcvq_ptr = (QTYPE *)queue;
+
+	int i=1; //consume counter
+	while (true) {
+		Byte *res, *dummy;
+		res = q_get(rcvq_ptr, dummy);
+		if(res){
+			printf("Mengkonsumsi byte ke-%d : '%c'\n",i,*res);
+			i++;
+		}
+		if(rcvq_ptr->count == 0 && end)
+		{
+			childead = true;
+			pthread_exit(0);
+		}
+		//usleep(DELAY); //delay
+	}
+	pthread_exit(0);
+}
---
+++ Luthfi/transmitter.cpp
@@ -0,0 +1,207 @@
+/*
+* File : transmitter.cpp
+* Author : Joshua K - 012, Albertus K - 100, Luthfi K -102
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <pthread.h>
+#include "dcomm.h"
+#include "crc16.h"
+
+/** FRAME THING **/
+int  append(char*s, size_t size, char c) {
+     if(strlen(s) + 1 >= size) {
+          return 1;
+     }
+     int len = strlen(s);
+     s[len] = c;
+     s[len+1] = '\0';
+     return 0;
+}
+#define SIZECHAR 5
+#define VARLEN 6
+
+/** WINDOW THING **/
+#define LISTSZ 100
+#define WINSIZE 5
+
+char listframe[100][1 + 1 + 1 + VARLEN + 1 + 2];
+int head = 0;
+
+/** GLOBAL VARIABLES **/
+char lastByteReceived = XON;
+int socket_desc;
+int isMainUp = 1;
+struct sockaddr_in server;
+char str_to_send[2];
+
+using namespace std;
+
+// Function declarations
+void *XON_XOFF_HANDLER(void *args);
+
+/**
+ * MAIN function
+ * sends bytes to receiver
+ */
+int main(int argc, char *argv[])
+{
+	/** Initialization **/
+	FILE* file;
+	int msg_len = 2;
+	int sock;
+	char* hostname = argv[1];
+	char* port = argv[2];
+	char* fileName = argv[3];
+
+	/** Load arguments **/
+	hostname = argv[1];
+	port = argv[2];
+	fileName = argv[3];
+
+	/** Socket **/
+	// creates socket
+    socket_desc = socket(AF_INET, SOCK_DGRAM, 0);
+    if (socket_desc == -1)
+    {
+        printf("Error: could not create socket");
+    }
+    
+    // initializes object server attributes
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = inet_addr(hostname);
+    server.sin_port = htons(atoi(port));
+
+	/** Initializes thread **/
+	pthread_t xon_xoff_handler;
+	// create thread xon_xoff_handler
+	if(pthread_create(&xon_xoff_handler, NULL, XON_XOFF_HANDLER, NULL) < 0)
+	{
+		perror("Error: could not create thread");
+		return 1;
+	}
+
+	printf("Membuat socket untuk koneksi ke %s:%s\n", hostname, port);
+
+	/** Sends bytes to receiver **/
+	// open file
+	file = fopen(fileName, "r");
+
+	if (file == NULL) {
+		perror("Error: could not open file");
+		exit(1);
+	}
+
+	// reads a line from the file and stores it into str_to_send
+	// reads (msg_len - 1) characters
+	// msg_len is 2 so that it only reads one character
+	int counter = 0, fnum = 1;
+	char frame[1 + 1 + 1 + VARLEN + 1 + 2] = "";
+	char text[VARLEN] = "";
+	char chks[3];
+	char etx[2]; etx[0] = ETX; etx[1] = 0;
+	while (fgets(str_to_send, msg_len, file) != NULL) {
+		if ((counter+1) % VARLEN == 0) {
+			frame[0] = SOH;
+			frame[1] = (char) fnum;
+			frame[2] = STX;
+			if (str_to_send[0] != '\n') { // mencegah karakter newline untuk ditransmisikan
+				strcat(text, str_to_send);
+				
+				
+				counter++;
+				
+				unsigned short ichks = calc_crc16(text, strlen(text));
+				printf("Mengirim byte ke-%d: '%s'\n", counter, text);
+				strcat(frame, text);
+				chks[0] = ichks & 0xff;
+				chks[1] = (ichks >> 8) & 0xff;
+				chks[2] = 0;
+				printf("Mengirim checksum '%s' from '%d'\n", chks, ichks);
+				strcat(frame, etx);
+				strcat(frame, chks);
+				printf("Mengirim frame '%s'\n", frame);
+				
+				strncpy(listframe[fnum], frame, 1 + 1 + 1 + VARLEN + 1 + 2);
+				printf("Copied frame '%s'\n", listframe[fnum]);
+				
+				sendto(socket_desc, frame, strlen(frame), 0, (struct sockaddr *)&server, sizeof(server));
+
+				// reset string
+				memset(str_to_send, 0, sizeof(str_to_send));
+				
+			}
+			memset(text, 0, sizeof text);
+			memset(frame, 0, sizeof frame);
+			memset(chks, 0, sizeof chks);
+			fnum++;
+		}
+		else {
+			if (str_to_send[0] != '\n') {
+				strcat(text, str_to_send);
+				counter++;
+			}
+		}
+	}
+	// mengirim endfile
+	frame[0] = SOH;
+	frame[1] = (char) fnum;
+	frame[2] = STX;
+	unsigned short ichks = calc_crc16(text, strlen(text));
+	printf("Mengirim byte ke-%d: '%s'\n", counter, text);
+	strcat(frame, text);
+	chks[0] = ichks & 0xff;
+	chks[1] = (ichks >> 8) & 0xff;
+	chks[2] = 0;
+	printf("Mengirim checksum '%s' from '%d'\n", chks, ichks);
+	strcat(frame, etx);
+	strcat(frame, chks);
+	printf("Mengirim frame '%s'\n", frame);
+	
+	strncpy(listframe[fnum], frame, 1 + 1 + 1 + VARLEN + 1 + 2);
+	printf("Copied frame '%s'\n", listframe[fnum]);
+				
+	sendto(socket_desc, frame, strlen(frame), 0, (struct sockaddr *)&server, sizeof(server));
+
+	str_to_send[0] = Endfile;
+	sendto(socket_desc, str_to_send, strlen(str_to_send), 0, (struct sockaddr *)&server, sizeof(server));
+	
+	isMainUp = 0;
+
+	printf("Reached end of file\n");
+	printf("Bye\n");
+	return 0;
+}
+
+
+/** THREAD
+ * 	receives XON/XOFF
+ */
+
+void *XON_XOFF_HANDLER(void *args) {
+
+	int rf;
+	int server_len = sizeof(server);
+	Byte recv_str[1];
+
+	while (true) {
+		// menunggu signal XON/XOFF
+		rf = recvfrom(socket_desc, recv_str, 1, 0, (struct sockaddr *)&server, (socklen_t*)&server_len);
+
+		if (rf < 0) {
+			perror ("Error: failed receiving XON/XOFF from socket");
+			exit(1);
+		}
+		// XON or XOFF
+		lastByteReceived = recv_str[0];
+		// reset
+		memset(recv_str, 0, sizeof(recv_str));
+	}
+
+	printf("Exit - XON/XOFF handler");
+	pthread_exit(0);
+}
